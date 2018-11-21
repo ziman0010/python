767cd00cbbc3983767919bda8039fdabbdf81bcc
unknown <IVC1-4@VLAN-25-14-233.rd.mirea.ru>
2018-11-21 17:44:44 +0300

    21.11.2018

--- lesson/blog/models.py
+++ lesson/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    description = models.TextField()
